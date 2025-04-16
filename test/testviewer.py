2ef2a93926ae6bfffe95066fe4db1533e187fd06
Jakub Aleksanderek <jogurt.truskawkowy@gmail.com>
2025-04-16 22:03:35 +0200

    Test WIP cd

--- test/testviewer.py
+++ test/testviewer.py
@@ -15,6 +15,7 @@ class TestViewer(unittest.TestCase):
 #    def tearDown(self):
 
     def test_is_viewer_action_correct_positive(self):
-        for action in {'e'}:
+        for action in {ord('e'), curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT, curses.KEY_HOME, curses.KEY_END}:
             self.assertTrue(is_viewer_action_correct(action))
+            
 
